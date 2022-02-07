d34cadf7a75e9c5d7e3c9a4b11fc228b4ef5fbfb
Mahmoud Dahdouh <mahmooddahdooh99@gmail.com>
2022-02-07 16:46:01 +0200

    mainmenu done

---
+++ app/src/main/java/com/example/funnytimesuserapp/SectionItems/FoodScreen.kt
@@ -0,0 +1,12 @@
+package com.example.funnytimesuserapp.SectionItems
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.funnytimesuserapp.R
+
+class FoodScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ft_screen_food)
+    }
+}
---
+++ app/src/main/java/com/example/funnytimesuserapp/SectionItems/ProductScreen.kt
@@ -0,0 +1,14 @@
+package com.example.funnytimesuserapp.SectionItems
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.funnytimesuserapp.R
+
+class ProductScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ft_screen_product)
+
+
+    }
+}
